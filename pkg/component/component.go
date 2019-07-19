5c9849bc3050bf835587cb2aadc134cdca14c38d
kroos.chen <kroos.chen@prophetstor.com>
2019-07-19 10:59:43 +0800

    change yaml namespaceService into go template

--- pkg/component/component.go
+++ pkg/component/component.go
@@ -1,8 +1,10 @@
 package component
 
 import (
+	"bytes"
 	"crypto/x509"
 	"fmt"
+	"html/template"
 	"net"
 	"strconv"
 	"strings"
@@ -77,7 +79,15 @@ func (c ComponentConfig) NewDaemonSet(str string) *appsv1.DaemonSet {
 		log.Error(err, "Failed to Test create DaemonSet")
 
 	}
-	d := resourceread.ReadDaemonSetV1(daemonSetBytes)
+	tmpl, err := template.New("namespaceServiceToYaml").Parse(string(daemonSetBytes[:]))
+	if err != nil {
+		panic(err)
+	}
+	yamlBuffer := new(bytes.Buffer)
+	if err = tmpl.Execute(yamlBuffer, c); err != nil {
+		panic(err)
+	}
+	d := resourceread.ReadDaemonSetV1(yamlBuffer.Bytes())
 	d.Namespace = c.NameSpace
 	d.Spec.Template = c.mutatePodTemplateSpecWithConfig(d.Spec.Template)
 	return d
@@ -140,7 +150,15 @@ func (c ComponentConfig) NewDeployment(str string) *appsv1.Deployment {
 		log.Error(err, "Failed to Test create deployment")
 
 	}
-	d := resourceread.ReadDeploymentV1(deploymentBytes)
+	tmpl, err := template.New("namespaceServiceToYaml").Parse(string(deploymentBytes[:]))
+	if err != nil {
+		panic(err)
+	}
+	yamlBuffer := new(bytes.Buffer)
+	if err = tmpl.Execute(yamlBuffer, c); err != nil {
+		panic(err)
+	}
+	d := resourceread.ReadDeploymentV1(yamlBuffer.Bytes())
 	d.Namespace = c.NameSpace
 	d.Spec.Template = c.mutatePodTemplateSpecWithConfig(d.Spec.Template)
 	return d
